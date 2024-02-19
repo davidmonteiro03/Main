4a18768f35b4daa2354e2e4c480e944a78f72909
davidmonteiro03 <damonteiro12446@gmail.com>
2024-02-19 14:39:09 +0000

    git updates

---
+++ CIC/winhost/cpas-junior-2014/08 - Cone/cone.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cone.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 11:36:21 by dcaetano          #+#    #+#             */
+/*   Updated: 2024/02/19 14:37:47 by dcaetano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+int get_length(int num, int n)
+{
+	if (num < 0)
+		num = -num;
+	if (num < 10)
+		return (n);
+	return (get_length(num / 10, n + 1));
+}
+
+int main(void)
+{
+	size_t i, j, num;
+	int length, tmp;
+	std::cin >> num;
+	length = get_length(num / 2 + num % 2, 1);
+	for (i = 0; i < num; i++)
+	{
+		for (j = 0; j < num; j++)
+		{
+			tmp = std::min(std::min(i, j), std::min(num - i - 1, num - j - 1));
+			tmp = num - tmp - (num / 2 + num % 2) + num % 2;
+			if (j > 0)
+				std::cout << " ";
+			printf("%*d", length, tmp);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
