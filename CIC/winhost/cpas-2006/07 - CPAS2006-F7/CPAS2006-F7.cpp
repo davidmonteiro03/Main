e40c367390f72ba714b156d3864c6d80c99ad3ed
davidmonteiro03 <damonteiro12446@gmail.com>
2024-02-20 11:57:01 +0000

    git updates

---
+++ CIC/winhost/cpas-2006/07 - CPAS2006-F7/CPAS2006-F7.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CPAS2006-F7.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 11:50:57 by dcaetano          #+#    #+#             */
+/*   Updated: 2024/02/20 11:56:57 by dcaetano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+	std::string::const_iterator it_a, it_b;
+	std::string a, b;
+	std::cin >> a >> b;
+	for (it_b = b.begin(); it_b != b.end(); it_b++)
+	{
+	}
+	return (0);
+}
